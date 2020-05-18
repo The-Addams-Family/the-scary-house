8b9766d4968e9a72b8f38cc57bbf5791e5ec976f
Fredrik <fredrik.persson@yh.nackademin.se>
2020-05-18 10:53:26 +0200

    Added resource and script for indoor

---
+++ Assets/GhostScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostScript : MonoBehaviour
+{
+    [SerializeField] private GameObject ghostObject;
+    [SerializeField] private float waitSeconds = 0.5f;
+    [SerializeField] private float fadeSteps = 0.05f;
+
+
+    private MeshRenderer _meshRenderer;
+    private Material _material;
+
+    private float _opacity; // synligheten för spöket
+
+    private IEnumerator _fadeIn;
+    private IEnumerator _fadeOut;
+
+    private void Start() {
+        _meshRenderer = ghostObject.GetComponent<MeshRenderer>();
+        _material = _meshRenderer.material;
+        Color c = _material.color;
+        c.a = 0;
+        _material.color = c;
+
+        _fadeIn = FadeIn();
+        _fadeOut = FadeOut();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            if (_fadeOut != null)
+                StopCoroutine(_fadeOut);
+            
+            StartCoroutine(FadeIn());
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "Player") {
+            if (_fadeIn != null)
+                StopCoroutine(_fadeIn);
+            StartCoroutine(FadeOut());
+        }        
+    }
+
+    IEnumerator FadeIn() {
+        Color c = _material.color;
+        for (float f = 0f; f <= 1.0f; f += fadeSteps) {
+            
+            c.a = f;
+            _material.color = c;
+            Debug.Log(f);
+            yield return new WaitForSeconds(waitSeconds);
+        }
+       
+    }
+
+    IEnumerator FadeOut() {
+         Color c = _material.color;
+        for (float f = 1.0f; f > 0.0f; f -= 0.1f) {
+           
+            c.a = f;
+            _material.color = c;
+            Debug.Log(f);
+            yield return new WaitForSeconds(waitSeconds);
+        }
+
+            c.a = 0.0f;
+            _material.color = c;        
+       
+    }    
+}
---
+++ Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+
+    [SerializeField] private float rotateRate;
+
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private float jumpSpeed = 10.0f;
+
+    [SerializeField] private float gravity = -9.82f;
+
+    [SerializeField] private float cornerTurnAngle = 0f;
+
+    [SerializeField] private float backAngle = 0f;
+    [SerializeField] private float forwardAngle = 180f;
+
+    private CharacterController _charController;
+
+    private float verticalVelocity = 0f;
+
+    private float verticalRotation = 90;
+
+    private float hAxis;
+
+    public float CornerTurnAngle {
+        get { return cornerTurnAngle; }
+         set { cornerTurnAngle = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       _charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        hAxis = Input.GetAxis("Horizontal"); // vänster/höger ska vara bak/fram 
+     
+        if (_charController.isGrounded) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                verticalVelocity = jumpSpeed;
+            } else {
+                verticalVelocity = 0f;
+            }
+        }
+
+                verticalVelocity += gravity * Time.deltaTime;
+        
+        /*
+         Move(hAxis);
+
+         Turn(hAxis);
+         */
+    
+        
+    }
+
+    private void FixedUpdate() {
+        /*
+        
+         Move(hAxis);
+
+         Turn(hAxis);
+        */
+    }
+
+    private void LateUpdate() {
+
+         Move(hAxis);
+
+         Turn(hAxis);        
+    }
+
+    private void Move(float input) {
+        
+        float dt = Time.deltaTime;
+
+       Quaternion rot = Quaternion.Euler(0f, cornerTurnAngle, 0f);
+        Vector3 forwardDirection = rot * Vector3.forward * input * moveSpeed * dt + Vector3.up * verticalVelocity * dt;
+        _charController.Move(forwardDirection);
+        
+        
+    }
+
+    private void Turn(float input) {
+         // Detta ser att rotera karaktären 180 grader när man byter flyttriktningen
+        if (input > 0.1f) {
+            verticalRotation = backAngle + cornerTurnAngle;
+        } else if (input < -0.1f) {
+            verticalRotation = forwardAngle + cornerTurnAngle;
+        }
+
+        Vector3 rot = transform.rotation.eulerAngles;
+        rot.y = Mathf.Lerp(rot.y, verticalRotation, 5f * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(rot);
+    }
+}
