dc4fae3c82e28f329ea2c9cf5d6744cbcf08689c
Fredrik <fredrik.persson@yh.nackademin.se>
2020-05-21 16:27:06 +0200

    merged with nicks

---
+++ Assets/Scripts/BrickCollide.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickCollide : MonoBehaviour
+{
+    Rigidbody rb;
+    PlayerLive player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerLive>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.name.Equals("Player")){
+            rb.isKinematic = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.name.Equals("Player")){
+            player.onDamage();
+            StartCoroutine(DestroyBrick());
+        }
+    }
+
+    IEnumerator DestroyBrick(){
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
+}
