fcc62ec66107889438451ba419569ffbf6642942
KodAnna <48121876+KodAnna@users.noreply.github.com>
2020-05-26 16:22:12 +0200

    update A-L
    Respawn point
    UI return to main menu button on canvas

--- Assets/IndoorScene/Scripts/Respawn.cs
+++ Assets/IndoorScene/Scripts/Respawn.cs
@@ -6,9 +6,21 @@ public class Respawn : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
+    private PlayerLive playerLive;
 
+
+    private void Start()
+    {
+        playerLive = GetComponent<PlayerLive>();
+    }
     private void OnTriggerEnter(Collider other)
     {
-        player.transform.position = respawnPoint.transform.position;
+        if (other.CompareTag("Player"))
+        {
+            playerLive.AnimateDeathText();
+
+        }
+        //    player.transform.position = respawnPoint.transform.position;
+        Debug.Log("Calling");
     }
 }
