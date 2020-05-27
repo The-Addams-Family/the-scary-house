69aba3a802f068b38d5d85a2a84409d213707e6c
KodAnna <48121876+KodAnna@users.noreply.github.com>
2020-05-27 12:07:54 +0200

    Pumkins
    Lagt in tre pumpor, ett tomt gameobjekt - pumkinhandler med ett script som har pumporna i en array

---
+++ Assets/IndoorScene/Scripts/LuckaRörSig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuckaRörSig : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+    // Set the x position to loop between 0 and 3
+    transform.position = new Vector3 (transform.position.x, Mathf.PingPong(Time.time, 4), transform.position.z);
+    }
+}
---
+++ Assets/IndoorScene/Scripts/PumpkinsMoving.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumpkinsMoving : MonoBehaviour
+{
+    [SerializeField] private GameObject[] movingPumkins;
+
+    private float[] speed = { 1.5f, 1f, 1.75f };
+
+    void Update()
+    {
+        MovingPumkins();
+
+        //transform.position = new Vector3(Mathf.PingPong(Time.time * speed, 5), transform.position.y, transform.position.z);
+        }
+
+    void MovingPumkins()
+    {
+        int count = 0;
+
+        foreach (var item in movingPumkins)
+        {
+            item.transform.position = new Vector3(Mathf.PingPong(Time.time * speed[count], 4), item.transform.position.y, item.transform.position.z);
+            count++;
+        }
+    }
+}
